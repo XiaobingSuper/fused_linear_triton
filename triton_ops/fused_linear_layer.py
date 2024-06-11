c2a2a9781ae54e6e23cb7527dc89c5d52f764135
XiaobingSuper <xiaobingzhangupc@gmail.com>
2024-06-11 11:01:36 +0800

    fuse relu with addm for fp32 path

--- triton_ops/fused_linear_layer.py
+++ triton_ops/fused_linear_layer.py
@@ -71,6 +71,51 @@ class _fused_linear_triton(torch.autograd.Function):
         return grad_input, grad_weight, grad_bias, None, None, None, None, None, None
 
 
+@torch.compile
+def relu_backward(grad_out, saved_act_y):
+    return torch.where(saved_act_y > 0, grad_out, 0)
+    return (saved_act_y > 0).float().mul(grad_out)
+
+class _fused_linear_torch(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx,
+        x,
+        weight,
+        bias,
+        trainable_weight,
+        trainable_bias
+    ):
+        # Kick the fused Triton kernel, handling bias and activation in one go
+        y = torch.ops.aten._addmm_activation(bias, x, weight.transpose(0, 1), beta=1, alpha=1)
+        ctx.trainable_weight = trainable_weight
+        ctx.trainable_bias = trainable_bias
+
+        # Micro-optimization: saving these is not always needed (?)
+        if x.requires_grad or ctx.trainable_weight or ctx.trainable_bias:
+            saved_act_y = y
+
+            ctx.save_for_backward(weight, x, saved_act_y)
+
+        return y
+
+    @staticmethod
+    def backward(
+        ctx: Any, grad_out: torch.Tensor
+    ) -> Any:  # pragma: no cover  # this is covered, but called directly from C++
+        """
+        Compute the derivative with respect to x, other tensors were not trainable inputs.
+        """
+        (weight, x, saved_act_y) = ctx.saved_tensors
+        # grad_out = torch.where(saved_act_y > 0, grad_out, 0)
+        grad_out = relu_backward(grad_out, saved_act_y)
+        # grad_out = relu_fusion_backward(grad_out, saved_act_y)
+        grad_input = grad_out @ weight
+        grad_weight = grad_out.transpose(1, 0) @ x if ctx.trainable_weight else None
+        grad_bias = grad_out.sum(0) if ctx.trainable_bias else None
+        return grad_input, grad_weight, grad_bias, None, None
+
+
 class FusedLinear(nn.Module):
     """
     Handle a linear transform, like torch.nn.Linear_, and a given activation, in a single kernel.
@@ -128,6 +173,14 @@ class FusedLinear(nn.Module):
                 self.training and x.requires_grad and self._activation_index > 1,
             )
         else:
+            if self._activation_index == 1 and x.dim() == 2:
+                return _fused_linear_torch.apply(
+                    x,
+                    weight,
+                    self.bias,
+                    self.weight.requires_grad,
+                    self.bias.requires_grad if self.bias is not None else False,
+                )
             out = F.linear(x, self.weight, self.bias)
             if self._activation_index > 0 :
                 if self._activation_index == 1:
